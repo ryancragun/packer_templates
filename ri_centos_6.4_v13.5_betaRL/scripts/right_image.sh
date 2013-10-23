199621422661ef7e16485a08e608c547ce455e0a
Ryan Cragun <ryan@rightscale.com>
2013-10-23 14:19:30 -0700

    fixed Epel repo and changed name

--- ri_centos_6.4_v13.5_betaRL/scripts/right_image.sh
+++ ri_centos_6.4_v13.5_betaRL/scripts/right_image.sh
@@ -3,11 +3,22 @@ curl -Ss 'https://s3.amazonaws.com/rightscale_key_pub/rightscale_key.pub' > /etc
 gpg --quiet --with-fingerprint /etc/pki/rpm-gpg/RPM-GPG-KEY-rightscale
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-rightscale
 
+# add Epel repo
+cat >/etc/yum.repos.d/Epel.repo<<-'EOF'
+[epel-mirror]
+name = Epel Mirror
+baseurl = http://cf-mirror.rightscale.com/epel/6/$basearch/archive/latest/
+failovermethod=priority
+gpgcheck=1
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rightscale
+EOF
+
 # add RightScale-epel repo
 cat >/etc/yum.repos.d/RightScale-epel.repo<<-'EOF'
 [rightscale-epel]
 name=RightScale Software
-baseurl=http://cf-mirror.rightscale.com/rightscale_software/epel/6/x86_64/archive/latest/
+baseurl=http://cf-mirror.rightscale.com/rightscale_software/epel/6/$basearch/archive/latest/
 gpgcheck=1
 enabled=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rightscale
